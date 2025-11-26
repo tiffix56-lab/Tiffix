261f4f035a8ac6ae29a3ebc4539a859cd9ddf897
Harsh <harshgupta20220@gmail.com>
2025-11-26 16:54:20 +0530

    fix

--- server/src/service/databaseService.js
+++ server/src/service/databaseService.js
@@ -5,8 +5,6 @@ export default {
     connect: async () => {
         try {
             await mongoose.connect(config.database.url);
-
-            await mongoose.connection.db.collection('subscriptions').dropIndex('planMenus_1');
             return mongoose.connection;
         } catch (err) {
             throw err;
