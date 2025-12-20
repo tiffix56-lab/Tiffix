7355366993784ee1ebc959abb5f15e76233c8f4d
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air.local>
2025-12-20 17:24:24 +0530

    feat: Ios

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,9 +1,10 @@
 
 const { getDefaultConfig } = require('@expo/metro-config');
-const { withNativeWind } = require('nativewind/metro-plugin');
+const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
   input: './src/global.css',
 });
+
